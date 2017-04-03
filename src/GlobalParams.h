a17aa6bad751b39ec1aa1ea7e500aa99bb698393
Vit <alfredfid@gmail.com>
2017-04-03 18:26:19 +0300

    changes

--- src/GlobalParams.h
+++ src/GlobalParams.h
@@ -6,9 +6,3 @@
 #else 
 #define IMAGE_PATH                        "C:/Users/Alfred/Desktop/TR/testData/BasicExample.jpg"
 #endif
-
-// debug parameters 
-#define VISUALIZATION			          (1)
-
-// algorithmic parameters 
-#define COMPOUND_TEXT_PARAMETER           (100)
--- src/SwapFace.h
+++ src/SwapFace.h
@@ -1,30 +1,17 @@
-/* TextRecognition class declaration */
+/* SwapFace class declaration */
 
 #include <opencv2\opencv.hpp>
 #include "GlobalParams.h"
 
-class TextRecognizer {
+class SwapFace {
 private:
 	cv::Mat fullFrame;
-	std::vector<std::pair<cv::RotatedRect, cv::Rect>> textStringsBoundingRects;
-	std::vector<cv::Mat> alignedTextStrings;
-
-	cv::Mat gainTextRegions(cv::Mat& frame);
-	void findTextStringsBoundingRects(cv::Mat& frame);	
-	void alignTextStrings();
-	cv::Mat processTextString(cv::Mat textStringMat);
-	std::string recognizeTextString();
 
 public:
-	TextRecognizer(cv::Mat& inputFrame) 
-		: fullFrame(inputFrame)
-		, textStringsBoundingRects(0, std::make_pair(cv::RotatedRect(), cv::Rect()))
-	    , alignedTextStrings(0, cv::Mat()) {};
+	SwapFace(cv::Mat& inputFrame) 
+		: fullFrame(inputFrame) {};
 
-	virtual ~TextRecognizer();
+	virtual ~SwapFace() {};
 
 	void run();
 };
-
-// additional functions 
-bool compareByMC(const std::vector<cv::Point> &a, const std::vector<cv::Point> &b);
